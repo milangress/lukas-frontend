0bf14d1f1f2c1622170bacd91f4397c2d72e565e
Milan G <dev@milangress.de>
2021-11-12 20:27:45 +0100

    ✨ setup link kat

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <template>
-  <Tutorial/>
+  <div>
+    <li v-for="link of links" :key="link.slug">
+      <NuxtLink :to="link.slug">{{ link.title }}</NuxtLink>
+    </li>
+  </div>
 </template>
 
 <script>
 export default {
+  async asyncData({ $content }) {
+    const links = await $content("links").fetch()
+
+    return {
+      links,
+    };
+  },
   head() {
     return {
       script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
